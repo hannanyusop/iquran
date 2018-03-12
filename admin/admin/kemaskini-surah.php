006fa1f5b18c4dc723495601e2ebaf5ee6848466
hannanyusop <nan_s96@yahoo.com>
2018-03-12 22:55:54 +0800

    uploading file

--- admin/admin/kemaskini-surah.php
+++ admin/admin/kemaskini-surah.php
@@ -12,24 +12,25 @@
 //check if surah exist
 if(isset($_GET['surah']) && isset($_GET['ayat'])){
 
-
     $surah_name = "TIADA";
-    $check_surah = "SELECT * FROM DaftarSurat as a LEFT JOIN ArabicQuran as b on a.index = b.surat WHERE surat_malaysia='$_GET[surah]' AND b.ayat = '$_GET[ayat]'";
+    $check_surah = "SELECT * FROM DaftarSurat as a 
+LEFT JOIN MalaysianQuran as c ON a.index=c.surat
+LEFT JOIN  ArabicQuran as b ON a.index=b.surat
+WHERE a.surat_malaysia='$_GET[surah]' AND b.ayat='$_GET[ayat]' AND c.ayat='$_GET[ayat]'";
     $fetch_surah = mysqli_query($db,$check_surah);
+//    print_r($fetch_surah); die();
 
     if ($fetch_surah) {
 
         $data = mysqli_fetch_assoc($fetch_surah);
-        print_r($data);
-        die();
         $surah_name = $data['surat_arab'].'('.$data['surat_malaysia'].')';
     }else{
 
-        echo '<script>alert("Ayat tidak wujud!");window.location="lihat-surah.php?surah=<?php echo $_GET[surah]; ?>"</script>';
+        echo '<script>alert("Ayat tidak wujud!");window.location="lihat-surah.php?surah='.$_GET[surah].'"</script>';
     }
 
 }else{
-    echo '<script>alert("Sila Pilih Ayat!");window.location="lihat-surah.php?surah=<?php echo $_GET[surah]; ?>"</script>';
+    echo '<script>alert("Sila Pilih Ayat!");window.location="lihat-surah.php?surah='.$_GET[surah].'"</script>';
 }
 
 ?>
@@ -54,72 +55,100 @@ if(isset($_GET['surah']) && isset($_GET['ayat'])){
                 </div>
                 <div class="col-md-6">
                     <div class="panel-content">
-                        <h2 class="heading"><i class="fa fa-square"></i> Basic Validation</h2>
-                        <form id="basic-form" method="post" novalidate="">
+                        <form id="basic-form" method="post">
+
+                            <h2 class="heading"><i class="fa fa-square"></i>Arab</h2>
+                            <?php
+                            $arabic = mysqli_query($db,"SELECT * FROM ArabicQuran as a WHERE a.surat='$_GET[surah]' AND  a.ayat='$_GET[ayat]'");
+
+                            if ($arabic) {
+
+                                $arabic_data = mysqli_fetch_assoc($arabic);
+                            ?>
+                                <div class="form-group">
+                                    <textarea class="form-control" rows="5" cols="30" disabled><?php echo $arabic_data['text']; ?></textarea>
+                                </div>
+                            <?php } ?>
+
+                            <br>
+                            <h2 class="heading"><i class="fa fa-square"></i>Terjemahan</h2>
+                            <?php
+                            $malay = mysqli_query($db,"SELECT * FROM MalaysianQuran as a WHERE a.surat='$_GET[surah]' AND  a.ayat='$_GET[ayat]'");
+
+                            if ($malay) {
+
+                                $malay_data = mysqli_fetch_assoc($malay);
+                                ?>
+                                <div class="form-group">
+                                    <textarea class="form-control" rows="5" name="mal_quran" cols="30" required><?php echo $malay_data['text']; ?></textarea>
+                                </div>
+                            <?php } ?>
+
+                            <h2 class="heading"><i class="fa fa-square"></i>Audio</h2>
                             <div class="form-group">
-                                <label>Text Input</label>
-                                <input type="text" class="form-control" required="">
-                            </div>
-                            <div class="form-group">
-                                <label>Email Input</label>
-                                <input type="email" class="form-control" required="">
-                            </div>
-                            <div class="form-group">
-                                <label>Text Area</label>
-                                <textarea class="form-control" rows="5" cols="30" required=""></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label>Checkbox</label>
-                                <br>
-                                <label class="fancy-checkbox">
-                                    <input type="checkbox" name="checkbox" required="" data-parsley-errors-container="#error-checkbox" data-parsley-multiple="checkbox">
-                                    <span>Option 1</span>
-                                </label>
-                                <label class="fancy-checkbox">
-                                    <input type="checkbox" name="checkbox" data-parsley-multiple="checkbox">
-                                    <span>Option 2</span>
-                                </label>
-                                <label class="fancy-checkbox">
-                                    <input type="checkbox" name="checkbox" data-parsley-multiple="checkbox">
-                                    <span>Option 3</span>
-                                </label>
-                                <p id="error-checkbox"></p>
-                            </div>
-                            <div class="form-group">
-                                <label>Radio Button</label>
-                                <br>
-                                <label class="fancy-radio">
-                                    <input type="radio" name="gender" value="male" required="" data-parsley-errors-container="#error-radio" data-parsley-multiple="gender">
-                                    <span><i></i>Male</span>
-                                </label>
-                                <label class="fancy-radio">
-                                    <input type="radio" name="gender" value="female" data-parsley-multiple="gender">
-                                    <span><i></i>Female</span>
-                                </label>
-                                <p id="error-radio"></p>
-                            </div>
-                            <div class="form-group">
-                                <label for="food">Multiselect</label>
-                                <br>
-                                <select id="food" name="food[]" class="multiselect multiselect-custom" multiple="multiple" data-parsley-required="" data-parsley-trigger-after-failure="change" data-parsley-errors-container="#error-multiselect" style="display: none;" data-parsley-multiple="food[]">
-                                    <option value="cheese">Cheese</option>
-                                    <option value="tomatoes">Tomatoes</option>
-                                    <option value="mozarella">Mozzarella</option>
-                                    <option value="mushrooms">Mushrooms</option>
-                                    <option value="pepperoni">Pepperoni</option>
-                                    <option value="onions">Onions</option>
-                                </select><div class="btn-group"><button type="button" class="multiselect dropdown-toggle btn btn-default" data-toggle="dropdown" title="None selected"><span class="multiselect-selected-text">None selected</span> <b class="caret"></b></button><ul class="multiselect-container dropdown-menu"><li><a tabindex="0"><label class="checkbox"><input type="checkbox" value="cheese"> Cheese</label></a></li><li><a tabindex="0"><label class="checkbox"><input type="checkbox" value="tomatoes"> Tomatoes</label></a></li><li><a tabindex="0"><label class="checkbox"><input type="checkbox" value="mozarella"> Mozzarella</label></a></li><li><a tabindex="0"><label class="checkbox"><input type="checkbox" value="mushrooms"> Mushrooms</label></a></li><li><a tabindex="0"><label class="checkbox"><input type="checkbox" value="pepperoni"> Pepperoni</label></a></li><li><a tabindex="0"><label class="checkbox"><input type="checkbox" value="onions"> Onions</label></a></li></ul></div>
-                                <p id="error-multiselect"></p>
+                                <?php
+                                $audio = mysqli_query($db,"SELECT * FROM track as a WHERE a.surat='$_GET[surah]' AND  a.ayat='$_GET[ayat]'");
+                                $audio_data = mysqli_fetch_assoc($audio);
+                                $audio_count = mysqli_num_rows ( $audio );
+
+                                if ($audio_count != 0 ) {
+                                    ?>
+                                    <audio controls>
+                                        <source src='../../track/<?php echo "$audio_data[surat]/$audio_data[ayat].mp3"; ?>' type='audio/mp3'>
+                                        Your browser does not support the audio tag.
+                                    </audio>
+                                <?php }else { ?>
+
+                                    <div class="alert alert-warning">
+                                        Tiada Audio
+                                        <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#uploadTrack" data-whatever="@mdo">Tambah Audio</button>
+                                    </div>
+
+                                <?php } ?>
                             </div>
                             <br>
-                            <button type="submit" class="btn btn-primary">Validate</button>
+                            <button type="submit" class="btn btn-primary">Update</button>
+
                         </form>
+
+                        <!-- MODAL -->
+
+                        <div class="modal fade" id="uploadTrack"  role="dialog" >
+                            <div class="modal-dialog" role="document">
+                                <form class="modal-content" enctype="multipart/form-data" method="post" action="upload-audio.php">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Tambah Audio</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">×</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                                <div class="row setup-content" id="step-1">
+                                                    <div class="col-xs-12">
+                                                        <div class="col-md-12 well text-center">
+                                                            <label for="fileToUpload">Sila pilih track (.mp3)</label><br />
+                                                            <input type="file" name="track"/>
+                                                            <input type="hidden" name="surah" value="<?php echo $_GET['surah']; ?>">
+                                                            <input type="hidden" name="ayat" value="<?php echo $_GET['ayat']; ?>">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                        <button type="submit" class="btn btn-primary">Kemaskini</button>
+                                    </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
         </div>
     </div>
+
 
     <!-- END MAIN CONTENT -->
     <div class="clearfix"></div>
